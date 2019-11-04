f78f7d2c1a72dda2deb93ff14f4453afb3dafeae
roerohan <rohan.mukherjee2018@vitstudent.ac.in>
2019-11-04 22:09:05 +0530

    Feat: TrainNo added to model

--- Train_BackEnd/models/seats.model.js
+++ Train_BackEnd/models/seats.model.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose'); // Erase if already required
 var seatSchema = new mongoose.Schema({
     seat: Number,
     passenger: String,
+    trainNo: String,
 });
 
 //Export the model
